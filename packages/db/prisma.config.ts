3084663ab268a494e95e195d2763d701a3808a60
Hoàng Dương Khánh <hdkhanh462@gmail.com>
2026-09-03 09:50:46 +0700

    feat: update environment configurations for server and web applications

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import { defineConfig, env } from "prisma/config";
 
 dotenv.config({
-  path: "../../apps/server/.env",
+  path: "../../apps/server/.env.dev",
 });
 
 export default defineConfig({
